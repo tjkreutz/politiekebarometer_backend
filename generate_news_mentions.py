6d95eaf9de8fc3c8a306c454e915404ca2660a24
Tim Kreutz <tjkreutz@gmail.com>
2019-03-18 12:46:48 +0100

    Move nlp modules to separate file. Improve detection.

--- generate_news_mentions.py
+++ generate_news_mentions.py
@@ -1,44 +1,28 @@
 import sys
-from operator import itemgetter
-from collections import defaultdict
-from pattern.text.nl import sentiment
-from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.tokenize import sent_tokenize
 
+from src import nlp
 from src import database
 from src import functions
 
-def pol_party_in_text(pol_party, text):
-    return pol_party.data['short_name'] in text.split()
-
-def pol_person_in_text(pol_person, text):
-    return pol_person.data['full_name'] in text
-
-def detect_theme(themes, text):
-    theme_count = defaultdict(int)
-    for theme, queries in themes.items():
-        theme_count[theme] += sum([text.count(query) for query in queries])
-    sorted_themes = [t for t, v in sorted(theme_count.items(), key=itemgetter(1), reverse=True) if v>0]
-    detected_theme = sorted_themes[0] if sorted_themes else None
-    return detected_theme
-
-def detect_polarity(text):
-    return sentiment(text)[0]
+FLEMISH = ['De Standaard', 'Het Nieuwsblad', 'Gazet van Antwerpen', 'Het Belang van Limburg', 'De Morgen', 'Het Laatste Nieuws', 'De Tijd', 'Metro', 'Knack', 'Trends', 'Humo', 'Krant van West-Vlaanderen', 'De Zondag', 'tPallieterke']
 
 def main(fps):
     db = database.get_database()
     cur = db.cursor()
 
-    themes = functions.load_themes('20140718_dutchdictionary_v2.lcd.txt')
+    themes = functions.load_themes('db/20140718_dutchdictionary_v2.lcd.txt')
 
-    #todo make one
     pol_parties = database.dbitems_from_table(cur, 'pol_parties')
     pol_persons = database.dbitems_from_table(cur, 'pol_persons')
 
     for fp in fps:
         for line in functions.jsonl_iterator(fp):
+            if not line['source'] in FLEMISH:
+                continue
             doc = database.DBItem('doc_all', {
                 'ts': functions.timestamp_to_datetime(float(line['published'])),
-                'theme_code': detect_theme(themes, line['text']),
+                'theme_code': nlp.detect_theme(themes, line['text']),
                 'url': line['url'],
             })
             news = database.DBItem('doc_news', {
@@ -49,18 +33,18 @@ def main(fps):
             for sentence in sent_tokenize(line['text'], language='dutch'):
                 fragment = database.DBItem('fragments', {
                     'content': sentence,
-                    'sentiment': detect_polarity(sentence)
+                    'sentiment': nlp.detect_polarity(sentence)
                 })
 
                 for pol_party in pol_parties:
-                    if pol_party_in_text(pol_party, sentence):
+                    if nlp.detect_pol_party_in_news(pol_party, sentence):
                         mention = database.DBItem('mentions', {
                             'pol_id': pol_party.data['pol_id']
                         })
                         insert = (doc, news, fragment, mention)
                         database.commit_news_mention(cur, insert)
                 for pol_person in pol_persons:
-                    if pol_person_in_text(pol_person, sentence):
+                    if nlp.detect_pol_person_in_news(pol_person, sentence):
                         mention = database.DBItem('mentions', {
                             'pol_id': pol_person.data['pol_id']
                         })
---
+++ src/nlp.py
@@ -0,0 +1,59 @@
+import json
+from numpy import mean
+from string import punctuation
+from operator import itemgetter
+from collections import defaultdict
+from nltk.tokenize import word_tokenize
+
+CHECK = punctuation + ' '
+SENT_LEXICON = json.load(open('db/sentiment.json'))
+
+def detect_pol_party_in_text(pol_party, text):
+    condition = 0
+    parts = text.split(pol_party.data['short_name'])
+    if len(parts)<2:
+        return False
+    if not parts[0]:
+        condition += 1
+    elif parts[0][-1] in CHECK:
+        condition +=1
+    if not parts[1]:
+        condition += 1
+    elif parts[1][0] in CHECK:
+        condition +=1
+    return condition==2
+
+def detect_pol_party_in_news(pol_party, news):
+    if pol_party.data['short_name'] in news:
+        return detect_pol_party_in_text(pol_party, news)
+    return False
+
+def detect_pol_party_in_tweet(pol_party, tweet):
+    if pol_party.data['twitter']:
+        if pol_party.data['twitter'] in tweet:
+            return True
+    if pol_party.data['short_name'] in tweet:
+        return detect_pol_party_in_text(pol_party, tweet)
+    return False
+
+def detect_pol_person_in_news(pol_person, news):
+    return pol_person.data['full_name'] in news
+
+def detect_pol_person_in_tweet(pol_person, tweet):
+    if pol_person.data['twitter']:
+        return pol_person.data['full_name'] in tweet or pol_person.data['twitter'] in tweet
+    return pol_person.data['full_name'] in tweet
+
+def detect_theme(themes, text):
+    theme_count = defaultdict(int)
+    for theme, queries in themes.items():
+        theme_count[theme] += sum([text.count(query) for query in queries])
+    sorted_themes = [t for t, v in sorted(theme_count.items(), key=itemgetter(1), reverse=True) if v>0]
+    detected_theme = sorted_themes[0] if sorted_themes else None
+    return detected_theme
+
+def detect_polarity(text):
+    tokens = word_tokenize(text, language='dutch')
+    polarities = [SENT_LEXICON[token] for token in tokens if token in SENT_LEXICON]
+    return mean(polarities) if polarities else 0
+
