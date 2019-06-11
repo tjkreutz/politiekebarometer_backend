8bbd996d3600dee4b4f02e2ec16f6817664dae8a
Tim Kreutz <tjkreutz@gmail.com>
2019-06-11 15:28:00 +0200

    Remove unused sql files

--- db/empty.sql
+++
@@ -1,8 +0,0 @@
-DELETE FROM mentions;
-ALTER TABLE mentions AUTO_INCREMENT = 1;
-DELETE FROM fragments;
-ALTER TABLE fragments AUTO_INCREMENT = 1;
-DELETE FROM doc_tweets;
-DELETE FROM doc_news;
-DELETE FROM doc_all;
-ALTER TABLE doc_all AUTO_INCREMENT = 1;
